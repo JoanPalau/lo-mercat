66a27a848d90bb75f2c30b0cc7ed873c1e3b3b11
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2022-10-17 11:05:16 +0200

    admin js test

---
+++ lo-mercat-app/adminjs/adminjs.js
@@ -0,0 +1,20 @@
+const AdminJS = require('adminjs')
+const AdminJSExpress = require('@adminjs/express')
+const express = require('express')
+
+const PORT = 3000
+
+const start = async () => {
+  const app = express()
+
+  const admin = new AdminJS({})
+
+  const adminRouter = AdminJSExpress.buildRouter(admin)
+  app.use(admin.options.rootPath, adminRouter)
+
+  app.listen(PORT, () => {
+    console.log(`AdminJS started on http://localhost:${PORT}${admin.options.rootPath}`)
+  })
+}
+
+start()
