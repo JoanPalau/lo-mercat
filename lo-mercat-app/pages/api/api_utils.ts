536d872ae74ba846b1b181171d344da703292fcf
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2022-10-24 19:40:11 +0200

    update or create example

---
+++ lo-mercat-app/pages/api/api_utils.ts
@@ -0,0 +1,37 @@
+interface UpdateOrCreatePrisma<T> {
+    schema: T
+    where: any
+    update: any
+    create: any
+}
+
+interface UpdateOrCreateReturn<T> {
+    created: boolean
+    obj: T
+}
+
+async function updateOrCreate<T>({ schema, where, update, create }: UpdateOrCreatePrisma<T>): Promise<UpdateOrCreateReturn<T>> {
+    let created = true;
+    let pre_defined = await (schema as any).findFirst(
+        {
+            where
+        }
+    );
+
+    let id = '0';
+    if (pre_defined != null) {
+        id = pre_defined.id;
+        created = false;
+    }
+
+    let obj = await (schema as any).upsert({
+        where: {
+            id: id
+        },
+        update,
+        create
+    });
+    return { created, obj };
+}
+
+export { updateOrCreate };
