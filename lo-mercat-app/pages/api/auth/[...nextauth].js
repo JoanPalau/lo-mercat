56fe7adaff25aecedde64e7ca6bfbdfe68411a3b
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2022-11-07 18:22:03 +0100

    nextauth to js

--- lo-mercat-app/pages/api/auth/[...nextauth].js
+++ lo-mercat-app/pages/api/auth/[...nextauth].js
@@ -18,8 +18,7 @@ export default NextAuth({
         type: 'credentials',
         credentials:{},
         async authorize(credentials,req){
-            const{email,password, role}=credentials as {email: string; password: string; role: string};
-
+            const{email,password, role}=credentials;
 
             if(email !== "didac@exemple.com" || password!=="1234"){
                 throw new Error('Ep! Credencials invalides!')
