ff6652f045aa97da95c613e333e578bd34e114c3
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2022-11-07 19:05:55 +0100

    NEXT AUTH key and wrap in ssr provider

--- lo-mercat-app/pages/_app.tsx
+++ lo-mercat-app/pages/_app.tsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react";
+import { SSRProvider } from 'react-bootstrap';
 import React, { useState } from 'react';
 import BarraNavegacio from './Components/Navbar';
 
@@ -13,14 +14,16 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
   const [user, setUser] = useState(null);
 
   return (
-    <div>
-      <UserContext.Provider value={{user, setUser}}>
-          <SessionProvider session={session}>
-            <BarraNavegacio/>
-              <Component {...pageProps} />
-          </SessionProvider>
-      </UserContext.Provider>
-    </div>
+    <SSRProvider>
+      <div>
+        <UserContext.Provider value={{user, setUser}}>
+            <SessionProvider session={session}>
+              <BarraNavegacio/>
+                <Component {...pageProps} />
+            </SessionProvider>
+        </UserContext.Provider>
+      </div>
+    </SSRProvider>
    );
 }
 export {UserContext};
