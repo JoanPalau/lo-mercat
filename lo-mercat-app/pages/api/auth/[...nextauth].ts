140b0f8c1587c3ae952b291142767c157409e6eb
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2022-11-07 20:46:53 +0100

    fix prisma nextauth

--- lo-mercat-app/pages/api/auth/[...nextauth].ts
+++ lo-mercat-app/pages/api/auth/[...nextauth].ts
@@ -2,12 +2,12 @@ import NextAuth, {NextAuthOptions, Session} from "next-auth";
 import CredentialsProvider from 'next-auth/providers/credentials'
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import prisma from "../../../prisma/lib/prismadb"
 import { NextResponse } from "next/server";
 import { withAuth } from "next-auth/middleware";
 import { GOOGLE_FONT_PROVIDER } from "next/dist/shared/lib/constants";
 import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 
 export default NextAuth({
     session:{
@@ -23,10 +23,14 @@ export default NextAuth({
         credentials:{},
         async authorize(credentials,req){
           const{email,password, role}=credentials as {email: string; password: string; role: string};
-
-          const user = await prisma.user.findFirst({
-            where: { email: email , password:password },
-          });
+          try{
+            const user = await prisma.user.findFirst({
+              where: { email: email , password:password },
+            });
+            console.log("E; " + email + " p: " + password + " u " + user);
+          } catch (e)  {
+            console.log("E; " + e);
+          }
     }}),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
