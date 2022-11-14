138aa9e5cc92fc3c7626576081f6ac5ab6e4a607
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2022-11-14 18:20:46 +0100

    first postman

--- lo-mercat-app/pages/api/stand/index.ts
+++ lo-mercat-app/pages/api/stand/index.ts
@@ -11,14 +11,17 @@ export default async function entrypoint(req: NextApiRequest, res: NextApiRespon
     } = req
 
     console.log("[LOG] " + method + " with query " + JSON.stringify(req.query));
+    let farmerId = farmer_id as string;
+    let marketId = market_id as string;
+    let stand = null;
+    console.log('XXXXXXXXXXX');
     //res.status(200).json({hello:'world'});
     switch (method) {
         case 'PUT':
-            let farmerId = farmer_id as string;
-            let marketId = market_id as string;
-    
+            
+            console.log('HOLAAAAAAAAAAAAAAAAAAAAAAAAAAA');
             // Update or create data in your database
-            let stand = await updateOrCreate({
+            stand = await updateOrCreate({
                 schema: prisma.stand,
                 where: {
                     farmerId,
@@ -33,6 +36,28 @@ export default async function entrypoint(req: NextApiRequest, res: NextApiRespon
             }
             );
 
+            res.status(200).json({ stand })
+            break
+        case 'DELETE':
+            console.log('adeuuuuuuuuuuuuuuuuuuuuuuuuu');
+            // Update or create data in your database
+            let findstand = await prisma.stand.findFirst({
+                where: {
+                    farmerId,
+                    marketId,
+                },
+            }
+            );
+            if(findstand!=null){
+                stand = await prisma.stand.delete({
+                    where: {
+                        id:findstand.id,
+                    },
+                }
+                );
+    
+            }
+            console.log("REMOVE");
             res.status(200).json({ stand })
             break
         default:
