6cc62e0ad5cffcff264dc70b455f2f4eff016cc3
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2022-11-14 18:46:29 +0100

    parameters in body

--- lo-mercat-app/pages/api/products/index.ts
+++ lo-mercat-app/pages/api/products/index.ts
@@ -6,17 +6,17 @@ const prisma = new PrismaClient();
 
 export default async function entrypoint(req: NextApiRequest, res: NextApiResponse) {
     const {
-        query: { name, customby },
+        query: { },
         method,
     } = req
 
-    console.log("[LOG] " + method + " with query " + JSON.stringify(req.query));
+    let Name = req.body.name as string;
+    let Customby = req.body.customby as string;
+    
+    console.log("[LOG] " + method + " with query " + JSON.stringify(req.query) + " and body " + JSON.stringify(req.body));
     //res.status(200).json({hello:'world'});
     switch (method) {
         case 'PUT':
-            let Name = name as string;
-            let Customby = customby as string;
-            
             // Update or create data in your database
             let product = await updateOrCreate({
                 schema: prisma.product,
