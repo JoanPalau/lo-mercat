47cf25954fd489266e2b8bfe6656f8257c6b0b47
CristianColavito <cristian.colavito@gmail.com>
2022-11-15 17:01:01 +0100

    add new api

--- lo-mercat-app/pages/api/purchase/index.ts
+++ lo-mercat-app/pages/api/purchase/index.ts
@@ -3,54 +3,37 @@ import { PrismaClient} from '@prisma/client';
 import { updateOrCreate } from '../api_utils';
 const prisma = new PrismaClient();
 
-
 export default async function entrypoint(req: NextApiRequest, res: NextApiResponse) {
     const {
         query: { },
         method,
     } = req
     console.log("[LOG] " + method + " with query " + JSON.stringify(req.query) + " and body " + JSON.stringify(req.body));
-    let name = req.body.name as string;
-    let location = req.body.location as string;
-    let schedule = req.body.schedule as string;
-    let type = req.body.type as string;
-    let id = req.body.id as string;
-    let market = null;
+    let created_at = req.body.created_at as string;
+    let customerId = req.body.customerId as string;
+    let purchase = null;
     //res.status(200).json({hello:'world'});
     switch (method) {
         case 'POST':
             let val : any = {
-                name,
-                location,
-                schedule,
-                type,
+                created_at,
+                customerId,
             }; 
             // Update or create data in your database
-            market = await prisma.market.create({
+            purchase = await prisma.purchase.create({
                 data: val
             }
             );
 
-            res.status(200).json(market)
+            res.status(200).json(purchase)
             break
-        case 'DELETE':
-            // Update or create data in your database
-            
-                market = await prisma.market.delete({
-                    where: {
-                        id,
-                    },
-                }
-                );
-            console.log("REMOVE");
-            res.status(204).json({});
-            break
+        
         case 'GET':
             // Get Stands
-            let findmarkets = await prisma.market.findMany();
+            let findpurchase = await prisma.purchase.findMany();
             
             console.log("GET");
-            res.status(200).json(findmarkets)
+            res.status(200).json(findpurchase)
             break
         default:
             res.setHeader('Allow', ['PUT'])
