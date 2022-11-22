76c4f8efa22d9e06be2ab0a74e9ff3904d8f7d6d
Colodidac <didaccolominas@gmail.com>
2022-11-22 19:55:21 +0100

    protected with cards

--- lo-mercat-app/pages/protected.tsx
+++ lo-mercat-app/pages/protected.tsx
@@ -4,6 +4,13 @@ import { useSession } from "next-auth/react";
 import Router from "next/router";
 import React, { FC, useContext, useEffect } from "react";
 import { UserContext } from "./_app";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import marketstock from '/Marketplace-bro.png';
+import Image from 'next/image';
 
 interface Props {
   children: React.ReactNode;
@@ -30,11 +37,96 @@ const Protected: FC<Props> = ({ children}): JSX.Element => {
         >
         Hola {session.user?.name}
         <p>
-        <Link href="/addstock">Manage Farmer</Link></p>
+        <Card sx={{
+            display: 'block',
+            width: '15vw',
+            transitionDuration: '0.3s',
+            height: '14vw'
+        }}>
+          <CardActionArea>
+            <Link href="/addstock">
+              <Image src={"/add-product.svg"} alt="" width={270} height={150} />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Add Stock
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Acces to a page to add stock of a product
+                </Typography>
+              </CardContent>
+            </Link>
+          </CardActionArea>
+        </Card>
+        </p>
         <p>
-        <Link href="/joinmarket">Join Market</Link></p>
+          <Card sx={{
+              display: 'block',
+              width: '15vw',
+              transitionDuration: '0.3s',
+              height: '14vw'
+          }}>
+            <CardActionArea>
+              <Link href="/addstock">
+                <Image src={"/farmer-info.svg"} alt="" width={280} height={150} />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Join to a Market
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Farmer, come join to one of our market!!
+                  </Typography>
+                </CardContent>
+              </Link>
+            </CardActionArea>
+          </Card>
+        </p>
         <p>
-        <Link href="/cristian/marketinfo">Market List</Link>
+          <Card sx={{
+              display: 'block',
+              width: '15vw',
+              transitionDuration: '0.3s',
+              height: '14vw'
+          }}>
+            <CardActionArea>
+              <Link href="/cristian/marketinfo">
+                <CardContent>
+                  <Image src={"/green-market-list.svg"} alt="" width={265} height={150} />
+                </CardContent>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Market List
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Take a look to our markets
+                  </Typography>
+                </CardContent>
+              </Link>
+            </CardActionArea>
+          </Card>
+        </p>
+        <p>
+          <Card sx={{
+              display: 'block',
+              width: '15vw',
+              transitionDuration: '0.3s',
+              height: '14vw'
+          }}>
+            <CardActionArea>
+              <Link href="https://http.cat/203">
+                <CardContent>
+                  <Image src={"/graphics.svg"} alt="" width={265} height={150} />
+                </CardContent>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Control Area
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Take a look to your Market Information
+                  </Typography>
+                </CardContent>
+              </Link>
+            </CardActionArea>
+          </Card>
         </p>
       </Grid>
     );
