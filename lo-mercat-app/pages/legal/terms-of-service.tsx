4dd22cd1723a00a9d1deefc51b5ca39f1d61a4b5
Joan Palau <joanp4l4u@gmail.com>
2022-11-22 19:10:54 +0100

    completed terms of service skeleton

--- lo-mercat-app/pages/legal/terms-of-service.tsx
+++ lo-mercat-app/pages/legal/terms-of-service.tsx
@@ -2,14 +2,51 @@ import { ReactElement } from 'react'
 
 import { GetStaticPropsContext } from 'next';
 
+import { Typography } from '@mui/material';
 import Layout from '@common/Layout';
 import type { NextPageWithLayout } from '@customTypes/NextPageWithLayout';
 
-const TermsOfService: NextPageWithLayout = () => {
+type Props = {
+  messages: {
+    TermsOfService: {
+      sections: [
+        {
+          title: string,
+          content: [string]
+        }
+      ]
+    }
+  }
+  ,
+  context: {}
+}
+
+const TermsOfService: NextPageWithLayout<Props> = (props) => {
+
+  const { messages } = props;
+  const t = messages["TermsOfService"];
+
   return (
-    <div>
-      Hello World ToS!
-    </div>
+    <>
+    {
+        t["sections"].map(
+          (section: { title: string, content: [string] }, index: number) => {
+            return (
+              <section key={index}>
+                <Typography variant="h5" component="h2" gutterBottom={true}>
+                  {section.title}
+                </Typography>
+                {
+                  section.content.map(
+                    (element, index) => <Typography key={index} align='justify' gutterBottom={true} paragraph={true} variant="body1">{element}</Typography>
+                  )
+                }
+              </section>
+            );
+          }
+        )
+      }
+    </>
   )
 }
 
