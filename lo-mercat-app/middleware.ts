c714c42932145df3d9f1899b52bcc58c6f11ed1e
Josep Maria Salvia Hornos <josh.salvia@gmail.com>
2022-11-23 11:35:39 +0100

    language buttons

--- lo-mercat-app/middleware.ts
+++ lo-mercat-app/middleware.ts
@@ -4,7 +4,6 @@ import { Role } from "@prisma/client";
 import { withAuth } from "next-auth/middleware";
 
 import type { NextRequest } from 'next/server'
-
 const ROLES_ALLOWED_TO_AUTH = new Set<Role>([
   Role.ADMIN,
   Role.USER,
@@ -34,5 +33,26 @@ export default withAuth(
 });
 
 export const config = {
-  matcher: ["/admin/:path*", "/nonAdminButSecure/:path*"],
+  // matcher: ["/admin/:path*", "/nonAdminButSecure/:path*"],
+  matcher: ["/(.*)"],
 };
+
+const PUBLIC_FILE = /\.(.*)$/
+
+export async function middleware(req: NextRequest) {
+  if (
+    req.nextUrl.pathname.startsWith('/_next') ||
+    req.nextUrl.pathname.includes('/api/') ||
+    PUBLIC_FILE.test(req.nextUrl.pathname)
+  ) {
+    return
+  }
+
+  const locale = req.cookies.get('NEXT_LOCALE')
+  if (locale != undefined && req.nextUrl.locale != locale.value) {
+    console.log('Redirecting from: ' + req.nextUrl.locale + ' to: ' + `/${locale.value}${req.nextUrl.pathname}${req.nextUrl.search}`)
+    return NextResponse.redirect(
+      new URL(`/${locale.value}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
+    )
+  }
+}
