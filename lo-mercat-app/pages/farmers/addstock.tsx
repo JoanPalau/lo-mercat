d703bddda7f753dadf1f428c79bd9cc7727d42bc
CristianColavito <cristian.colavito@gmail.com>
2022-11-30 20:34:32 +0100

    add the warning button when u rem a stock

--- lo-mercat-app/pages/farmers/addstock.tsx
+++ lo-mercat-app/pages/farmers/addstock.tsx
@@ -5,19 +5,20 @@ import { NextPageWithLayout } from '@customTypes/NextPageWithLayout';
 import { ReactElement } from 'react';
 import { isMobile } from '@common/DeviceDetection';
 import { NextPageContext } from 'next';
-
+import { getSession, useSession } from 'next-auth/react';
 import { prisma } from '../../lib/prisma';
 
 export async function getServerSideProps(context: NextPageContext) {
     // Fetch data from external API
     //const res = await fetch('http://localhost:3000/api/hello')
     // const data = {};
+    const session= await getSession(context);
     const product = await prisma.product.findMany();
     
     const stocks = await prisma.stock.findMany({
         where: {
             farmer: {
-                id: '1',
+                id: session.farmer.id,
             },
         },
         include:{
@@ -39,7 +40,7 @@ const AddProductStockPage: NextPageWithLayout = ({ product, stocks,props}: any) 
     return (
         <div>
             <AddProductForm product={product}{...props} />
-            <ViewProductStock stock={stocks}/>
+            <ViewProductStock stock={stocks}{...props}/>
         </div>
     );
 }
